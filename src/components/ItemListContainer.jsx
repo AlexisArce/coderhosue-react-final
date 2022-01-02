f6addf6f61df67dcff3e11aa1eb8e8edb8ad6fe0
aarce <Arce.Alexis@hotmail.com>
2022-01-02 02:08:49 -0300

    Add filter by category

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,30 +1,36 @@
 import React, { useState, useEffect } from "react";
-//import { useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import ItemList from "./ItemList";
 import Spinner from "./Spinner";
-import { collection, getFirestore, getDocs, query } from "firebase/firestore";
+import {
+  collection,
+  getFirestore,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
 
 const ItemListContainer = () => {
   const [items, setItems] = useState([]);
-  //const { category } = useParams();
+  const { category } = useParams();
 
   useEffect(() => {
     const db = getFirestore();
-    const queryCollection = query(
-      collection(db, "products")
-      //where("category", "=", category)
-    );
+    const queryCollection = category
+      ? query(collection(db, "products"), where("category", "==", category))
+      : query(collection(db, "products"));
+
     getDocs(queryCollection)
       .then((resp) => {
         console.log(resp);
         setItems(resp.docs.map((prod) => ({ id: prod.id, ...prod.data() })));
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [category]);
 
   return (
     <div className="container fluid my-4">
-      {items && items.length ? <ItemList items={items} /> : <Spinner />}
+      {items ? <ItemList items={items} /> : <Spinner />}
     </div>
   );
 };
